e16806fb71254dc4c3fb7426edc49647edebd824
DaCodeChick <peepvtuber@gmail.com>
2025-06-06 23:41:24 -0700

    recover typedefs.h

--- src/psyq/types.h
+++ src/psyq/types.h
@@ -1,10 +1,4 @@
 #pragma once
 
-#ifdef _WIN32
-#define EXPORT __declspec(dllexport)
-#else
-#define EXPORT __attribute__((visibility("default")))
-#endif
-
 typedef unsigned short u_short; ///< Unsigned 16-bit integer
 typedef unsigned long u_long;   ///< Unsigned 32-bit integer
